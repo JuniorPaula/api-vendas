8424e342230a39cdef82e351e50d0a841c88bd09
Junior Paula <slucivaldo509@gmail.com>
2022-01-04 19:45:54 -0300

    customization application error handling

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,9 @@
+/** Class de error da aplicação */
+class AppError {
+  constructor(readonly message: string, readonly statusCode: number | 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 dotenv.config();
@@ -12,6 +13,23 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+/** middleware para tratamento de erros */
+app.use(
+  (error: Error, requeste: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+);
 
 const port = process.env.PORT;
 app.listen(port, () => {
