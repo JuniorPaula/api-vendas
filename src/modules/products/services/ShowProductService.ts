8f00253fce72c9acb60668786cfdf6348029a862
Junior Paula <slucivaldo509@gmail.com>
2022-01-05 14:54:49 -0300

    create  service methos to list one products by id

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Products';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRopository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
