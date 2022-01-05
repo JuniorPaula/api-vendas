364e97f2305b4d5308bede8ffdf08314e57a4dfc
Junior Paula <slucivaldo509@gmail.com>
2022-01-05 14:51:26 -0300

    create  service methos to list all products

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Products';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRopository';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
