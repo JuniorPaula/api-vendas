84d2208babc64c646b15f93aff1c72c514aee96b
Junior Paula <slucivaldo509@gmail.com>
2022-01-05 15:08:38 -0300

    create  service methos to update one products by id

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,44 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Products';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRopository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    const productExists = await productsRepository.findByname(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;
