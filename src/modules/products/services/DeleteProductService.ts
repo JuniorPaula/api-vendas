9054f32e7e4d4447c615164874079f44fd347d92
Junior Paula <slucivaldo509@gmail.com>
2022-01-05 15:17:19 -0300

    create  service methods to delete one product

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '../typeorm/repositories/ProductsRopository';
+
+interface IRequest {
+  id: string;
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
