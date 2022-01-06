6e65d49ecf42a58ce73d90ffd8ddbcf70aed2a89
Junior Paula <slucivaldo509@gmail.com>
2022-01-06 11:14:07 -0300

    install and configuare multer to upload files

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -5,7 +5,6 @@ import ListUserService from '../services/ListUserService';
 export default class UserController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listUser = new ListUserService();
-    console.log(request.user.id);
     const users = await listUser.execute();
 
     return response.json(users);
