a24bf249dfe9fd339a2d270789dac995ac25aefd
Junior Paula <slucivaldo509@gmail.com>
2022-01-06 19:53:07 -0300

    create service to show profile user

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowProfileService {
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+
+    const user = await userRepository.findById(user_id);
+
+    if (!user) throw new AppError('User not found.');
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
