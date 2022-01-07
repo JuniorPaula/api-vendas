d491d5d62dcddf62e67179c8a93043e299c80495
Junior Paula <slucivaldo509@gmail.com>
2022-01-07 14:38:32 -0300

    implament paginate of the customer list service

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,6 +4,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 
 import AppError from '@shared/errors/AppError';
 import '../typeorm';
@@ -16,6 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(pagination);
 
 app.use('/files', express.static(uploadConfig.directory));
 
