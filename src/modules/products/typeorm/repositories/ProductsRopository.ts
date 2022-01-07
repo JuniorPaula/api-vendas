4abb8929e9aae980f5ad6d7086ce1d8f4e14c7b5
Junior Paula <slucivaldo509@gmail.com>
2022-01-07 13:38:32 -0300

    create orderServices

--- src/modules/products/typeorm/repositories/ProductsRopository.ts
+++ src/modules/products/typeorm/repositories/ProductsRopository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Products';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductsRepository extends Repository<Product> {
@@ -11,5 +15,17 @@ export class ProductsRepository extends Repository<Product> {
     });
 
     return product;
+  }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const productIds = products.map((product) => product.id);
+
+    const existsProducts = await this.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return existsProducts;
   }
 }
