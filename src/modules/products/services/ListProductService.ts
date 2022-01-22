47f8e5b38b1d335c793f1aeb356f5b9772ef96cf
Junior Paula <slucivaldo509@gmail.com>
2022-01-22 09:58:32 -0300

    implements Redis on class ListProductsServices

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,4 +1,5 @@
 import { getCustomRepository } from 'typeorm';
+import RedisCache from '@shared/cache/RedisCache';
 import Product from '../typeorm/entities/Products';
 import { ProductsRepository } from '../typeorm/repositories/ProductsRopository';
 
@@ -6,7 +7,20 @@ class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductsRepository);
 
-    const products = await productsRepository.find();
+    const redisCache = new RedisCache();
+
+    let products = await redisCache.recover<Product[]>(
+      'api-vendas-PRODUCTS_LIST'
+    );
+
+    /** verificar se o redis tem produtos armazenados, se não salva
+     * na base de dados e cria o cache no Redis
+     */
+    if (!products) {
+      products = await productsRepository.find();
+
+      await redisCache.save('api-vendas-PRODUCTS_LIST', products);
+    }
 
     return products;
   }
