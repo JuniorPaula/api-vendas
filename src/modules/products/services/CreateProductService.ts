d70251d6f8fdfd97c6bf728e58254256c32624da
Junior Paula <slucivaldo509@gmail.com>
2022-01-22 10:04:56 -0300

    implements Redis all services of the products

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Products';
@@ -19,11 +20,15 @@ class CreateProductService {
       throw new AppError('There is already one product with this name');
     }
 
+    const redisCache = new RedisCache();
+
     const product = productsRepository.create({
       name,
       price,
       quantity,
     });
+
+    await redisCache.invalidate('api-vendas-PRODUCTS_LIST');
 
     await productsRepository.save(product);
 
--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import { ProductsRepository } from '../typeorm/repositories/ProductsRopository';
@@ -15,6 +16,10 @@ class DeleteProductService {
     if (!product) {
       throw new AppError('Product not found');
     }
+
+    const redisCache = new RedisCache();
+
+    await redisCache.invalidate('api-vendas-PRODUCTS_LIST');
 
     await productsRepository.remove(product);
   }
